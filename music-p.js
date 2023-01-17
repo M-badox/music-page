8461c96eb91e3c886c62bf0cb2da32033443dd28
M-badox <adelain1212@gmail.com>
2023-01-17 20:46:38 +0200

    Add Necessary Comment In The Js File

--- music-p.js
+++ music-p.js
@@ -1,16 +1,18 @@
 let sound=document.getElementById("sound");
 let icon=document.getElementById("icon");
-
+//creating function that will change pause to play and vice-versa
 icon.onclick=function()
 {
     if(sound.paused)
     {
+        //if paused then play and change the image
         sound.play();
         icon.src =
 "https://media.geeksforgeeks.org/wp-content/uploads/20210402235545/Pause.png";
     }
     else
     {
+        //if play then pause and change the image
        sound.pause();
        icon.src =
 "https://media.geeksforgeeks.org/wp-content/uploads/20210402235520/play.png";
